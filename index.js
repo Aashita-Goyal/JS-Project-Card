0ab6d31b9780ff2cc723a7dfa80efcaddf9c863c
Aashita-Goyal <aashitagoyal99@gmail.com>
2021-08-12 17:09:13 +0530

    Editable Task

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+//Parent element to store card
 const taskContainer = document.querySelector(".task__container");
 
+//Global store
 let globalStore = [];
 
 const generateNewCard = (taskData) => `
 <div class="col-md-6 col-lg-4">
                         <div class="card">
                             <div class="card-header d-flex justify-content-end gap-2">
-                                <button type="button" class="btn btn-success">
-                                <i class="fas fa-pencil-alt"></i>
+                                <button type="button" class="btn btn-success" id=${taskData.id} onclick= "editCard.apply(this, arguments)">
+                                <i class="fas fa-pencil-alt" id=${taskData.id} onclick= "editCard.apply(this, arguments)"></i>
                                 </button>
 
                                 <button type="button" class="btn btn-danger" id=${taskData.id} onclick= "deleteCard.apply(this, arguments)">
@@ -68,7 +70,8 @@ const saveChanges = () =>
 
 const deleteCard = (event) => {
 
-    event = window.event;
+    event = window.event;  //event contains all browser related properties (window is parent of browser)
+                           //It also contains the HTML element (of the card)
 
     //accessing id
     const targetID = event.target.id;
@@ -91,4 +94,45 @@ const deleteCard = (event) => {
 
 
 
+};
+
+
+const editCard = (event) => {
+    event = window.event; 
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;  //BUTTON
+
+    //let taskTitle = document.querySelector(".card-title");
+    // the above code has an issue. Here our code will read all cards alike.
+    //therefore, correcting thi issue, we write,
+
+    let parentElement;
+
+    if(tagname === "BUTTON"){
+        parentElement = event.target.parentNode.parentNode;
+    }else{
+        parentElement = event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+
+    //Now changing the Open task button to Save changes
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    submitButton.innerHTML = "Save Changes";
+
+
+
+   /* We log the parent elements to get the index of childnodes 
+   in console on webpage. But we do not need to show this to the user,
+   therefore console.log is disabled
+    console.log(taskTitle);
+    console.log(taskDescription);
+    console.log(taskType); */
 };

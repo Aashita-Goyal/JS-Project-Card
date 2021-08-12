d3b32ad68726461ab5f3fb61540a48ccbe935e99
Aashita-Goyal <aashitagoyal99@gmail.com>
2021-08-12 18:04:56 +0530

    Tasky - Task Management Site

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const generateNewCard = (taskData) => `
                               <a href="#" class="btn btn-primary">${taskData.taskType}</a>
                             </div>
                             <div class="card-footer ">
-                                <button type="button" class="btn btn-primary float-end" id="open__task">Open Task</button>
+                                <button type="button" id=${taskData.id} class="btn btn-primary float-end" id="open__task">Open Task</button>
                             </div> 
                           </div>
                       </div>
@@ -44,10 +44,11 @@ const loadInitialCardData = () => {
         //update out globalstore
         globalStore.push(cardObject);
     })
+};
 
-    
-
-}
+const updateLocalStorage = () => {
+    localStorage.setItem("tasky", JSON.stringify({ cards:globalStore }));  //an object
+};
 
 
 const saveChanges = () => 
@@ -64,7 +65,8 @@ const saveChanges = () =>
   
     globalStore.push(taskData);
 
-    localStorage.setItem("tasky", JSON.stringify({cards:globalStore})); 
+    //localStorage.setItem("tasky", JSON.stringify({cards:globalStore})); 
+    updateLocalStorage();
 };
 
 
@@ -79,7 +81,8 @@ const deleteCard = (event) => {
 
 
     globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
-    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));  //an object
+    //localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));  //an object
+    updateLocalStorage();
 
    // globalStore = newUpdatedArray;
 
@@ -125,7 +128,12 @@ const editCard = (event) => {
     //Now changing the Open task button to Save changes
     let submitButton = parentElement.childNodes[7].childNodes[1];
 
+   
     submitButton.innerHTML = "Save Changes";
+    submitButton.setAttribute(  //onvlivking the save changes button, it will enter SaveEditChanges
+        "onclick",
+        "saveEditChanges.apply(this, arguments)"
+    );
 
 
 
@@ -136,3 +144,81 @@ const editCard = (event) => {
     console.log(taskDescription);
     console.log(taskType); */
 };
+
+
+
+//Now we need to be able to save the changes done on editing
+
+const saveEditChanges = (event) => {
+    event = window.event; 
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;  
+
+    let parentElement;
+
+    if(tagname === "BUTTON"){
+        parentElement = event.target.parentNode.parentNode;
+    }else{
+        parentElement = event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    const updatedData = {
+        taskTitle: taskTitle.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+        taskType: taskType.innerHTML,
+    };
+
+    //console.log({ updatedData });  - to print the saved data in console
+
+    
+    //Our data is now being saved to local storage array
+    //Now we need to save it to the global store array as well
+    globalStore = globalStore.map((task) =>{
+        if (task.id === targetID){
+            return{
+                id: task.id,  //this is not being edited by us, it remains same
+                imageUrl: task.imageUrl, //this is also not being edited
+                taskTitle: updatedData.taskTitle,
+                taskDescription: updatedData.taskDescription,
+                taskType: updatedData.taskType,
+            }
+        }
+        return task;  //IMPORTANT 
+        /* the above return only returns the matched data. 
+                therefore we also NEED to write this return so that it returns 
+                all data, even the one that did not match the above 
+                condition i.e. the one which was NOT edited */
+    });
+
+    updateLocalStorage(); 
+    //console.log(globalStore);
+
+
+    /*Even after we are saving our changes, then, without clicking the
+    'editing pencil' icon again, we can still edit the data on our card.
+    However, we should Not be able to edit it again unless the pencil icon
+    is clicked. Therefore, solving this issue, we apply setAttribute 
+    contenteditable to false instead of tre */
+    taskTitle.setAttribute("contenteditable", "false");
+    taskDescription.setAttribute("contenteditable", "false");
+    taskType.setAttribute("contenteditable", "false");
+
+    //Now changing the Save Changes button BACK TO open task
+    submitButton.innerHTML = "Open Task";
+
+    //After saving, if we click the save changes/open task button again,
+    //it can again enter into the editing function, therefore,
+    //to remove that, we se removeAttribute for submitButton 
+    //(this happens after our edited changes are saved)
+    submitButton.removeAttribute = ("onclick");
+
+
+};
+
+
+
